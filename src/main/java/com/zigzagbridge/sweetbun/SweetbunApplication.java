1fe7e3a5230090fe4137067028c636ad1d062591
Keita Kasai <k013c1345@it-neec.jp>
2018-11-17 12:03:56 +0900

    created project by spring initializr

---
+++ src/main/java/com/zigzagbridge/sweetbun/SweetbunApplication.java
@@ -0,0 +1,12 @@
+package com.zigzagbridge.sweetbun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetbunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetbunApplication.class, args);
+    }
+}
